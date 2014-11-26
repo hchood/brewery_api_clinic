2bac9489f51b8898cd5485bc384475805755ce19
Helen Hood <helen.c.hood@gmail.com>
2014-11-26 15:42:55 -0500

    Add breweries index page

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv'
 require 'pry'
+require 'httparty'
 
 Dotenv.load
+# city = "Boston"
+# response = HTTParty.get("http://beermapping.com/webservice/locquery/#{ENV["BEER_MAPPING_API_KEY"]}/#{city}")
+
+get '/breweries' do
+  city = "Boston"
+  response = HTTParty.get("http://beermapping.com/webservice/locquery/#{ENV["BEER_MAPPING_API_KEY"]}/#{city}")
+  @breweries = response["bmp_locations"]["location"]
+
+  erb :'breweries/index'
+end
+
 
